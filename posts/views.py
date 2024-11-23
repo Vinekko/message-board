128541834f5721bf3584c2040fd4f05dfb33b9f2
Vinekko <nitomaco@gmail.com>
2024-11-23 10:01:10 -0400

    show-tasks done

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Publication
 # Create your views here.
+
+class PostListView(ListView):
+    model = Publication
+    template_name = 'post_list.html'
